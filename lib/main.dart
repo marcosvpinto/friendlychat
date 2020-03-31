16407e05194e9624fd43f0c7de1b833cd2c50b81
Marcos Pinto <mvpdev@gmail.com>
2020-03-31 18:56:52 -0300

    Ajuste para ambas as plataformas iOS e Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(
     new FriendlychatApp()
   );
 }
+
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey,
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 const String _name = "Your name";
 
@@ -13,6 +26,7 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS ? kIOSTheme : kDefaultTheme,
       home: new ChatScreen(),
     );
   }
@@ -83,10 +97,15 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
             ),
             new Container(
               margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(
+              child: Theme.of(context).platform == TargetPlatform.iOS ?
+                new CupertinoButton(
+                  child: new Text("Send"),
+                  onPressed: _isComposing ? () => _handleSubmitted(_textController.text) : null,
+                ) :
+                new IconButton(
                   icon: new Icon(Icons.send),
                   onPressed: _isComposing ? () => _handleSubmitted(_textController.text) : null
-              ),
+              ) ,
             ),
           ],
         ),
@@ -99,26 +118,29 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendlychat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(
-              child: new ListView.builder(
-                  padding: new EdgeInsets.all(8.0),
-                  reverse: true,
-                  itemBuilder: (_, int index) => _messages[index],
-                  itemCount: _messages.length,
+      body: new Container(
+              child: new Column(
+                children: <Widget>[
+                  new Flexible(
+                      child: new ListView.builder(
+                          padding: new EdgeInsets.all(8.0),
+                          reverse: true,
+                          itemBuilder: (_, int index) => _messages[index],
+                          itemCount: _messages.length,
+                      ),
+                  ),
+                  new Divider(height: 1.0),
+                  new Container(
+                    decoration: new BoxDecoration(
+                      color: Theme.of(context).cardColor
+                    ),
+                    child: _buildTextComposer(),
+                  ),
+                ],
               ),
-          ),
-          new Divider(height: 1.0),
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor
-            ),
-            child: _buildTextComposer(),
-          ),
-        ],
-      ),
+              decoration: Theme.of(context).platform == TargetPlatform.iOS ? new BoxDecoration(border: new Border(top: new BorderSide(color: Colors.grey[200]),),) : null),
     );
   }
 }
